0e31b46c60453a6ae46b17587664f40aa06ae8e5
aledepaulaaa <contatoaledepaula@gmail.com>
2024-08-12 20:26:28 -0300

    Ajuste em /pages/politicaeprivacidade

---
+++ src/pages/politicaeprivacidade/index.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material"
+
+export default function Poltiica(){
+    return (
+        <Box>
+            <Typography>
+                Politica de Privacidade
+            </Typography>
+        </Box>
+    )
+}
